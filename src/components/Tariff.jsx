e628f9a3ad4227f0da5109d8aad77641ed00147d
Evgeniia Kazantceva <evg.kazantseva@gmail.com>
2022-01-08 20:32:18 +0500

    add state

--- src/components/Tariff.jsx
+++ src/components/Tariff.jsx
@@ -1,21 +1,39 @@
-import './Tariff.scss';
-
+import "./Tariff.scss";
+import React, { useState } from "react";
 
 function Tariff(props) {
-  return (
-    <div className="tariff-card" id={props.id}>
-    <div className="tariff-name">Безлимитный {props.cost}
-      </div> 
+  const [checked, setChecked] = useState(false);
+  const handleClick = () => {
+    setChecked(!checked);
+  };
+
+  return checked ? (
+    <div className="tariff-card" id={props.id} onClick={handleClick}>
+      <div className="tariff-name selected-tariff-name">
+        Безлимитный {props.cost}
+      </div>
       <div className="tariff-cost">
-          <span className="tariff-cost_span-currency">руб</span> {props.cost} 
-          <span className="tariff-cost_span">{'/'}мес</span>
-      </div> 
-      <div className="tariff-value">до {props.value} Мбит/сек
-      </div> 
-      <div className="tariff-description">
-      Объем включенного траффика не ограничен</div>  
+        <span className="tariff-cost_span-currency">руб</span> {props.cost}
+        <span className="tariff-cost_span">{"/"}мес</span>
+      </div>
+      <div className="tariff-value">до {props.value} Мбит/сек</div>
+      <div className="tariff-description selected-tariff-description">
+        Объем включенного траффика не ограничен
+      </div>
     </div>
-  )
+  ) : (
+    <div className="tariff-card" id={props.id} onClick={handleClick}>
+      <div className="tariff-name">Безлимитный {props.cost}</div>
+      <div className="tariff-cost">
+        <span className="tariff-cost_span-currency">руб</span> {props.cost}
+        <span className="tariff-cost_span">{"/"}мес</span>
+      </div>
+      <div className="tariff-value">до {props.value} Мбит/сек</div>
+      <div className="tariff-description">
+        Объем включенного траффика не ограничен
+      </div>
+    </div>
+  );
 }
 
-export default Tariff
+export default Tariff;
